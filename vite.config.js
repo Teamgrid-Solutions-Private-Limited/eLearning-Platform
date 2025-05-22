926b72aaf1bf6fc7c59a93796a67aa41e5653e9b
Imran Ahmed <imran@stagingwebsite.link>
2025-05-22 13:46:29 +0530

    Test 1

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'/authoring-tool/',
+  server: {
+    host: 'localhost', // Change to '0.0.0.0' if you want external access
+    port: 5173,        // Change to your desired port (default is 5173)
+  },
 })
