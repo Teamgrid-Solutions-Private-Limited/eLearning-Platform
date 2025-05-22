03b37257cc11f0be0bc068bbed50dace31de966b
Imran Ahmed <imran@stagingwebsite.link>
2025-05-22 13:47:54 +0530

    test 2

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  base:'/authoring-tool/',
-  server: {
-    host: 'localhost', // Change to '0.0.0.0' if you want external access
-    port: 5173,        // Change to your desired port (default is 5173)
-  },
-})
+	plugins: [react()],
+	base: "/authoring-tool/",
+	server: {
+		host: "localhost",
+		port: 5173,
+		fs: {
+			allow: [".."],
+		},
+	},
+});
